c0bcc6847e2af348a30cda168d13889cd2022594
Rafał Łasocha <me+dev@swistak35.com>
2015-04-20 15:36:12 +0200

    jars

--- proglog/pracownia3/jars.pl
+++ proglog/pracownia3/jars.pl
@@ -6,44 +6,80 @@ jars(Caps, Target, Result) :-
   length(InitialState, Count),
   zero_list(InitialState),
   jars_solution(Caps, Target, [InitialState], 0, SomeResult, 1000),
-  !,
+  % !,
   findBetterSolution(Caps, Target, [InitialState], SomeResult, Result).
 
 findBetterSolution(Caps, Target, States, Limit, Result) :-
   jars_solution(Caps, Target, States, 0, BetterResult, Limit),
-  !,
+  % !,
   findBetterSolution(Caps, Target, States, BetterResult, Result).
-findBetterSolution(Caps, Target, States, Limit, Limit) :-
-  \+ jars_solution(Caps, Target, States, 0, _Result, Limit).
+findBetterSolution(Caps, Target, States, Limit, Limit).
+% findBetterSolution(Caps, Target, States, Limit, Limit) :-
+%   \+ jars_solution(Caps, Target, States, 0, _Result, Limit).
 
-jars_solution(_Caps, _Target, _States, Steps, _Result, Lim) :-
-  Steps >= Lim,
+% jars_solution(_Caps, _Target, _States, Steps, _Result, Lim) :-
+%   Steps >= Lim,
+%   !,
+%   fail.
+
+jars_solution(_Caps, _Target, [CurrentState|PreviousStates], Steps, _Result, Lim) :-
+  (Steps >= Lim; member(CurrentState, PreviousStates)),
   !,
   fail.
 
-jars_solution(_Caps, _Target, [CurrentState|PreviousStates], _Steps, _Result, _Lim) :-
-  member(CurrentState, PreviousStates),
-  !,
-  fail.
-
-jars_solution(_Caps, Target, [CurrentState|_TailStates], Result, Result, _Lim) :-
-  member(Target, CurrentState),
-  !. % Jesli juz znalezlismy, to nastepne na pewno beda dluzsze.
+jars_solution(Caps, Target, [CurrentState|TailStates], Result, Result, _Lim) :-
+  member(Target, CurrentState).
+  % add_new_solution(Caps, Target, TailStates, Result),
+  % !. % Jesli juz znalezlismy, to nastepne na pewno beda dluzsze.
 
 jars_solution(Caps, Target, [CurrentState|T], Steps, Result, Lim) :-
-  move(Caps, CurrentState, NewState),
+  \+ member(Target, CurrentState),
+  new_step(Caps, CurrentState, NewState),
   NSteps is Steps + 1,
   jars_solution(Caps, Target, [NewState, CurrentState|T], NSteps, Result, Lim).
 
-jars_solution(Caps, Target, [CurrentState|T], Steps, Result, Lim) :-
-  make_full(Caps, CurrentState, NewState),
-  NSteps is Steps + 1,
-  jars_solution(Caps, Target, [NewState, CurrentState|T], NSteps, Result, Lim).
+% add_new_solution(Caps, Target, [HState|TStates], Result) :-
+%   add_solution_for(Caps, Target, HState, Result),
+%   add_new_solution(Caps, Target, TStates, Result).
 
-jars_solution(Caps, Target, [CurrentState|T], Steps, Result, Lim) :-
-  make_empty(Caps, CurrentState, NewState),
-  NSteps is Steps + 1,
-  jars_solution(Caps, Target, [NewState, CurrentState|T], NSteps, Result, Lim).
+% add_solution_for(Caps, Target, HState, Result) :-
+%   old_solution(Caps, Target, HState, OldResult),
+%   Result < OldResult,
+%   asserta((old_solution(Caps, Target, HState, Result) :- !.)).
+% add_solution_for(Caps, Target, HState, Result) :-
+%   old_solution(Caps, Target, HState, OldResult),
+%   Result >= OldResult.
+
+  
+
+
+% jars_solution(Caps, Target, [CurrentState|T], Steps, Result, Lim) :-
+%   make_full(Caps, CurrentState, NewState),
+%   NSteps is Steps + 1,
+%   jars_solution(Caps, Target, [NewState, CurrentState|T], NSteps, Result, Lim).
+
+% jars_solution(Caps, Target, [CurrentState|T], Steps, Result, Lim) :-
+%   make_empty(Caps, CurrentState, NewState),
+%   NSteps is Steps + 1,
+%   jars_solution(Caps, Target, [NewState, CurrentState|T], NSteps, Result, Lim).
+
+% :- dynamic old_step / 3.
+
+% old_step(Caps, CurrentState, NewState) :- fail.
+
+% step(Caps, CurrentState, NewState) :-
+  % old_step(Caps, CurrentState, NewState).
+% step(Caps, CurrentState, NewState) :-
+%   new_step(Caps, CurrentState, NewState),
+  % asserta(old_step(Caps, CurrentState, NewState)).
+
+
+new_step(Caps, CurrentState, NewState) :-
+  make_full(Caps, CurrentState, NewState).
+new_step(Caps, CurrentState, NewState) :-
+  make_empty(Caps, CurrentState, NewState).
+new_step(Caps, CurrentState, NewState) :-
+  move(Caps, CurrentState, NewState).
 
 make_full([Cap|_TCaps], [_Val|TVals], [Cap|TVals]).
 make_full([_|TCaps], [Val|TVals], [Val|NTVals]) :-
