d2eeb1d2419d7508d87e0bbbdba7e46831b59eb0
Rafał Łasocha <me+dev@swistak35.com>
2015-04-27 15:23:22 +0200

    up

--- proglog/cwiczenia3/connected.pl
+++ proglog/cwiczenia3/connected.pl
@@ -8,9 +8,36 @@ edge(krakow, lodz).
 edge(siedlce, biala_podl). 
 edge(siedlce, warszawa).
 
-connected(X, Y) :-
-  connected2(X, Y, [X], []).
+revedge(X, Y) :- edge(Y, X).
 
-connected2(Source, Target, States, OldVisited) :-
+connected(X, Y) :-
+  connected2(Y, [X], []).
+
+remove([], _, []).
+remove([H|T], OldStates, T1) :-
+  member(H, OldStates),
+  remove(T, OldStates, T1).
+remove([H|T], OldStates, [H|T1]) :-
+  \+ member(H, OldStates),
+  remove(T, OldStates, T1).
+
+findalltargets(Source, Targets) :-
+  findall(Target, edge(Source, Target), Targets).
+
+connected2(Target, States, _OldVisited) :-
+  member(Target, States),
+  !.
+
+connected2(_Target, [], _OldVisited) :-
+  !,
+  fail.
+
+connected2(Target, States, OldVisited) :-
+  maplist(findalltargets, States, AllNewStates),
+  append(AllNewStates, AllNewStates2),
+  sort(AllNewStates2, UniqNewStates),
+  append(OldVisited, States, NewOldVisited),
+  remove(UniqNewStates, NewOldVisited, UniqNewStates2),
+  connected2(Target, UniqNewStates2, NewOldVisited).
 
 
