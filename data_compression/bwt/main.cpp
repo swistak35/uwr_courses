3f6816026ccde98d9d5c01165972058bff018291
Rafał Łasocha <me+dev@swistak35.com>
2015-05-14 00:47:24 +0200

    up

--- data_compression/bwt/main.cpp
+++ data_compression/bwt/main.cpp
@@ -6,7 +6,7 @@
 #include "MoveToFront.h"
 #include "DemoveToFront.h"
 
-#define TESTING_MODE true
+#define TESTING_MODE false
 
 using namespace std;
 
@@ -42,36 +42,52 @@ int main() {
     cout << endl;
 
     // DemoveToFront
-    char source3[source_len + 1] = { 0 };
+    char target2[source_len + 1] = { 0 };
     DemoveToFront * demtf = new DemoveToFront(source_len);
-    demtf->transform(mtf_tbl, source3);
-    cout << "Source3: " << source3 << endl;
+    demtf->transform(mtf_tbl, target2);
+    cout << "Target2: " << target2 << endl;
   } else {
     FILE * dane = fopen("wiersz.txt", "r");
     fseek(dane, 0, SEEK_END);
-    int dane_size = ftell(dane);
+    int source_len = ftell(dane);
     rewind(dane);
-    char * source = (char *) malloc(dane_size + 1);
-    source[dane_size] = 0;
-    fread(source, dane_size, 1, dane);
-    /* if (source[dane_size - 1] == 0) { */
+    char * source = (char *) malloc(source_len + 1);
+    source[source_len] = 0;
+    fread(source, source_len, 1, dane);
+    /* if (source[source_len - 1] == 0) { */
     /*   std::cout << "Istotnie jest 0!" << std::endl; */
     /* } else { */
     /*   std::cout << "Nie ma 0!" << std::endl; */
     /* } */
     fclose(dane);
-    char * target = (char *) malloc(dane_size + 1);
-    target[dane_size] = 0;
-    LexiBWT * bwt = new LexiBWT(dane_size);
+    char * target = (char *) malloc(source_len + 1);
+    target[source_len] = 0;
+    LexiBWT * bwt = new LexiBWT(source_len);
     int orig_idx = bwt->transform(source, target);
     std::cout << "Target: " << target << std::endl;
     std::cout << "Index of orig string: " << orig_idx << std::endl;
 
     // Decoding
-    char source2[dane_size + 1] = { 0 };
-    LexiDeBWT * debwt = new LexiDeBWT(dane_size);
+    char source2[source_len + 1] = { 0 };
+    LexiDeBWT * debwt = new LexiDeBWT(source_len);
     debwt->transform(orig_idx, target, source2);
     std::cout << "Source2: " << source2 << std::endl;
+
+    // MoveToFront
+    int mtf_tbl[source_len];
+    MoveToFront * mtf = new MoveToFront(source_len);
+    mtf->transform(target, mtf_tbl);
+    cout << "MTF:";
+    for (int i = 0; i < source_len; i++) {
+      cout << " " << mtf_tbl[i];
+    }
+    cout << endl;
+
+    // DemoveToFront
+    char target2[source_len + 1] = { 0 };
+    DemoveToFront * demtf = new DemoveToFront(source_len);
+    demtf->transform(mtf_tbl, target2);
+    cout << "Target2: " << target2 << endl;
   }
 
   return 0;
