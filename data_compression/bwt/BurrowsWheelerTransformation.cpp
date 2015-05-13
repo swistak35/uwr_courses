523d8de3e1bf55897c23ab7655be808227fc4ab2
Rafał Łasocha <me+dev@swistak35.com>
2015-05-13 14:05:30 +0200

    up

--- data_compression/bwt/BurrowsWheelerTransformation.cpp
+++ data_compression/bwt/BurrowsWheelerTransformation.cpp
@@ -14,38 +14,89 @@ int BurrowsWheelerTransformation::transform(char * source, char * target) {
   this->source = source;
   this->target = target;
 
+  initial_sort();
+}
+
+void BurrowsWheelerTransformation::initial_sort() {
+  std::vector<std::vector<int>> hvec(256, std::vector<int>(0));
+
+  std::vector<int> tmp_ranks;
+
   for (int i = 0; i < this->length; i++) {
-    this->ranks.push_back(i);
+    hvec[this->source[i]].push_back(i);
+    this->ranks.push_back(0);
     this->positions.push_back(i);
   }
-  
-  int offset = this->length - 1;
-  while (offset >= 0) {
-    vector<vector<int>> hvec(256, vector<int>(0));
 
-    for (int i = 0; i < this->length; i++) {
-      char * begin_of_this_string = this->source - this->positions[i];
-      if (begin_of_this_string < this->source) {
-        begin_of_this_string += this->length;
-      }
-
-      char * offseth_char_of_this_string = begin_of_this_string + offset;
-      if (offseth_char_of_this_string > source_end) {
-        offseth_char_of_this_string -= this->length;
-      }
-      // na razie wrzucamy tylko nr stringa
-      hvec[*offseth_char_of_this_string].push_back = this->positions[i];
+  for (int i = 0; i < 256; i++) {
+    /* std::cout << "Znak `" << i << "`: "; */
+    for (int v : hvec[i]) {
+      /* std::cout << v << " "; */
+      tmp_ranks.push_back(v);
     }
+    /* std::cout << std::endl; */
+  }
 
-    vector<int> new_order(this->length);
+  for (int i = 0; i < this->length; i++) {
+      this->ranks[tmp_ranks[i]] = i;
+      this->positions[i] = tmp_ranks[i];
+  }
 
-    for (auto hvec1 : hvec) {
-      for (int string_number : hvec1) {
-        new_order.push_back(string_number);
-      }
-    }
-
-
-    offset--;
+  for (int i = 0; i < this->length; i++) {
+    /* std::cout << i << ": " << this->ranks[i] << std::endl; */
+    this->target[this->ranks[i]] = this->source[i];
   }
 }
+
+void BurrowsWheelerTransformation::next_sort() {
+  int k = 0;
+  int offset = 1; // 2 ^ k
+
+  std::vector<std::vector<int>> hvec(256, std::vector<int>(0));
+
+  std::vector<int> tmp_ranks;
+
+  for (int i = 0; i < this->length; i++) {
+    char c = this->source[get_char_idx(this->positions[i] + offset)]
+    hvec[c].push_back(this->positions[i]);
+  }
+
+  for (int i = 0; i < 256; i++) {
+    /* std::cout << "Znak `" << i << "`: "; */
+    for (int v : hvec[i]) {
+      /* std::cout << v << " "; */
+      tmp_ranks.push_back(v);
+    }
+    /* std::cout << std::endl; */
+  }
+
+  for (int i = 0; i < this->length; i++) {
+    this->ranks[tmp_ranks[i]] = i;
+    this->positions[i] = tmp_ranks[i];
+  }
+
+  for (int i = 0; i < this->length; i++) {
+    /* std::cout << i << ": " << this->ranks[i] << std::endl; */
+    this->target[this->ranks[i]] = this->source[i];
+  }
+}
+
+void BurrowsWheelerTransformation::get_char_idx(int idx) {
+  if (idx >= this->length) {
+    return (idx - this->length + 1);
+  } else {
+    return idx;
+  }
+}
+
+/*
+ * 0    B A B A C A
+ * 1    A B A B A C
+ * 2    C A B A B A
+ * 3    A C A B A B
+ * 4    B A C A B A
+ * 5    A B A C A B
+ *
+ * A A A B B C
+ *
+ */
