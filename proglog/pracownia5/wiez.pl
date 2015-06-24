97c123016997ad22eaf3b1df0365706cc767729f
Rafał Łasocha <me+dev@swistak35.com>
2015-06-24 03:50:43 +0200

    up

--- proglog/pracownia5/wiez.pl
+++ proglog/pracownia5/wiez.pl
@@ -1,12 +1,37 @@
+:- dynamic mypermK/3.
+:- dynamic mypermW/3.
+
 wiezowce(N, W, K, R) :-
+  retractall(mypermK(_,_,_)),
+  retractall(mypermW(_,_,_)),
   genBoard(N, Board),
   transp(Board, TransposedBoard),
+  assert_all_perms(N, K, W),
   filling_row(Board, TransposedBoard, N, W, K),
   R = Board.
 
-fill_list(L, N) :-
-  numlist(1, N, K),
-  permutation(K, L).
+assert_all_perms(N, K, W) :-
+  numlist(1, N, L),
+  permutation(L, P),
+  count_mono(P, CL),
+  reverse(P, R),
+  count_mono(R, CR),
+  assert_for_mypermK(CL, CR, P, K),
+  assert_for_mypermW(CL, CR, P, W),
+  fail.
+assert_all_perms(N, K, W).
+
+assert_for_mypermK(CL, CR, P, K) :-
+  member((CL, CR), K),
+  assert(mypermK(CL, CR, P)),
+  !.
+assert_for_mypermK(CL, CR, P, K).
+assert_for_mypermW(CL, CR, P, W) :-
+  member((CL, CR), W),
+  assert(mypermW(CL, CR, P)),
+  !.
+assert_for_mypermW(CL, CR, P, W).
+
 
 %%% Transposing
 transp([T], X) :-
@@ -23,14 +48,15 @@ append_to_all([], [], []).
 append_to_all([H1|T1], [H2|T2], [[H1|H2]|T3]) :-
   append_to_all(T1, T2, T3).
 
-check_row(K, [H|T], Max) :-
+count_mono(L, Res) :- count_mono(L, 0, 0, Res).
+count_mono([], _Max, Acc, Acc).
+count_mono([H|T], Max, Acc, Res) :-
   H > Max,
   !,
-  K1 is K - 1,
-  check_row(K1, T, H).
-check_row(K, [_H|T], Max) :-
-  check_row(K, T, Max).
-check_row(0, [], _) :- !.
+  NAcc is Acc + 1,
+  count_mono(T, H, NAcc, Res).
+count_mono([H|T], Max, Acc, Res) :-
+  count_mono(T, Max, Acc, Res).
 
 check_row_right(K, L) :-
   reverse(L, L2),
@@ -38,16 +64,12 @@ check_row_right(K, L) :-
 
 filling_row([], _ColumnBoard, _N, _W, _K).
 filling_row([Current|RestRows], ColumnBoard, N, [(HLeft, HRight)|TW], K) :-
-  fill_list(Current, N),
-  check_row(HLeft, Current, 0),
-  check_row_right(HRight, Current),
+  mypermW(HLeft, HRight, Current),
   filling_col(RestRows, ColumnBoard, N, TW, K).
 
 filling_col(_RowBoard, [], _N, _W, _K).
 filling_col(RowBoard, [Current|RestCols], N, W, [(HTop, HBottom)|TK]) :-
-  fill_list(Current, N),
-  check_row(HTop, Current, 0),
-  check_row_right(HBottom, Current),
+  mypermK(HTop, HBottom, Current),
   filling_row(RowBoard, RestCols, N, W, TK).
 
 genBoard(N, Board) :- genBoard(0, N, Board).
