dfd3e894829477fcc8d678c1b74302b379f856ed
Rafał Łasocha <me+dev@swistak35.com>
2015-06-24 03:03:45 +0200

    up

--- proglog/pracownia5/wiez.pl
+++ proglog/pracownia5/wiez.pl
@@ -1,7 +1,7 @@
 wiezowce(N, W, K, R) :-
   genBoard(N, Board),
   transp(Board, TransposedBoard),
-  filling_row(Board, Board, TransposedBoard, N, W, K),
+  filling_row(Board, TransposedBoard, N, W, K),
   R = Board.
 
 fill_list(L, N) :-
@@ -26,33 +26,29 @@ append_to_all([H1|T1], [H2|T2], [[H1|H2]|T3]) :-
 check_row(K, [H|T], Max) :-
   H > Max,
   !,
-  % K1 is K - 1,
-  succ(K1, K),
-  K1 >= 0,
+  K1 is K - 1,
   check_row(K1, T, H).
 check_row(K, [_H|T], Max) :-
   check_row(K, T, Max).
 check_row(0, [], _) :- !.
 
-check_row_left(K, L) :-
-  check_row(K, L, 0).
 check_row_right(K, L) :-
   reverse(L, L2),
   check_row(K, L2, 0).
 
-filling_row(_Board, [], _ColumnBoard, _N, _W, _K).
-filling_row(Board, [Current|RestRows], ColumnBoard, N, [(HLeft, HRight)|TW], K) :-
+filling_row([], _ColumnBoard, _N, _W, _K).
+filling_row([Current|RestRows], ColumnBoard, N, [(HLeft, HRight)|TW], K) :-
   fill_list(Current, N),
-  check_row_left(HLeft, Current),
+  check_row(HLeft, Current, 0),
   check_row_right(HRight, Current),
-  filling_col(Board, RestRows, ColumnBoard, N, TW, K).
+  filling_col(RestRows, ColumnBoard, N, TW, K).
 
-filling_col(_Board, _RowBoard, [], _N, _W, _K).
-filling_col(Board, RowBoard, [Current|RestCols], N, W, [(HTop, HBottom)|TK]) :-
+filling_col(_RowBoard, [], _N, _W, _K).
+filling_col(RowBoard, [Current|RestCols], N, W, [(HTop, HBottom)|TK]) :-
   fill_list(Current, N),
-  check_row_left(HTop, Current),
+  check_row(HTop, Current, 0),
   check_row_right(HBottom, Current),
-  filling_row(Board, RowBoard, RestCols, N, W, TK).
+  filling_row(RowBoard, RestCols, N, W, TK).
 
 genBoard(N, Board) :- genBoard(0, N, Board).
 genBoard(N, N, []) :- !.
